bf59cc8f2d41523bf0f6502abf6834a61197e320
Phil Dunn <67956409+pdunn467@users.noreply.github.com>
2021-03-20 13:04:57 +0000

    Update name.groovy

--- name.groovy
+++ name.groovy
@@ -1,14 +1,14 @@
 class Example { 
    static void main(String[] args) { 
       // Initializing a local variable 
-      int os = RHEL7 
-      int type = patch 
-      int date = Date 
+      String os = "RHEL7"; 
+      String type = "patch";
+      String date = "Date"; 
 		
       //Check for the boolean condition 
-      if (os = RHEL7) { 
+      if (os == 'RHEL7') { 
          //If the condition is true print the following statement 
-         println("The name is "+ $os+" "+$date+" "+$type); 
+         println("The name is"); 
       } else { 
          //If the condition is false print the following statement 
          println("The value is greater than 100"); 
