4099779320840ad881503f26caef420250fef7fb
Phil Dunn <67956409+pdunn467@users.noreply.github.com>
2021-03-20 12:48:14 +0000

    Create name.groovy

---
+++ name.groovy
@@ -0,0 +1,17 @@
+class Example { 
+   static void main(String[] args) { 
+      // Initializing a local variable 
+      int os = RHEL7 
+      int type = patch 
+      int date = Date 
+		
+      //Check for the boolean condition 
+      if (os = RHEL7) { 
+         //If the condition is true print the following statement 
+         println("The name is "+ $os+" "+$date+" "+$type); 
+      } else { 
+         //If the condition is false print the following statement 
+         println("The value is greater than 100"); 
+      } 
+   } 
+}
