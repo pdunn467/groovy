22de0a589f1f0883039fa29e32dd7f9200643328
Phil Dunn <67956409+pdunn467@users.noreply.github.com>
2021-03-21 09:06:57 +0000

    Update name.groovy

--- name.groovy
+++ name.groovy
@@ -1,17 +1,20 @@
 class Example { 
    static void main(String[] args) { 
       // Initializing a local variable 
+      String prod = "false";
       String os = "RHEL7"; 
       String type = "patch";
       String date = "Date"; 
 		
       //Check for the boolean condition 
-      if (os == 'RHEL7') { 
+      if (prod == 'false') { 
          //If the condition is true print the following statement 
-         println("The name is " + os); 
+         buildname = "test-" + os + "-" + type;
+         println(buildname); 
       } else { 
          //If the condition is false print the following statement 
-         println("The value is greater than 100"); 
+         buildname = "test-CDE-" + os + "-" + type;
+         println(buildname); 
       } 
    } 
 }
